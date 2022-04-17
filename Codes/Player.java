7db7fe287fc0e4fdf51eb92901660a3fff4f0e56
siwakota <98551780+siwakota@users.noreply.github.com>
2022-04-17 13:51:25 -0400

    Player.java created

---
+++ Codes/Player.java
@@ -0,0 +1,40 @@
+/*
+**  This java class is for the 
+**  player's name, character and
+**  scores.
+*/
+
+public class Player 
+{
+    // Private data fields
+    private String name;
+    private String character;
+    private int Score;
+
+    
+    // Getters and setters for the private data fields.
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharacter() {
+        return this.character;
+    }
+
+    public void setCharacter(String sym) {
+        this.character=sym;
+    }
+
+    public int getScore() {
+        return this.Score;
+    }
+
+    public void setScore(int score) {
+        this.Score = score;
+    }
+    
+}
