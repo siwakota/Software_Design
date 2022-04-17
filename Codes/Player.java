2bcefe23049fdf0a9c1f0cab5ede38860ca432c0
siwakota <98551780+siwakota@users.noreply.github.com>
2022-04-17 16:20:15 -0400

    Added datafields , getters and setters

--- Codes/Player.java
+++ Codes/Player.java
@@ -1,5 +1,34 @@
 public class Player 
 {
-   //Codes for Player's properties and all goes in here.
+   // Private data fields
+    private String name;
+    private String character;
+    private int Score;
+
+    
+    // Getters and setters for the private data fields.
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharacter() {
+        return this.character;
+    }
+
+    public void setCharacter(String sym) {
+        this.character=sym;
+    }
+
+    public int getScore() {
+        return this.Score;
+    }
+
+    public void setScore(int score) {
+        this.Score = score;
+    }
     
 }
