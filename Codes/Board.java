84d4246753ea6e98f3e75b7f323c81050a89919c
karan5906 <98905430+karan5906@users.noreply.github.com>
2022-04-17 16:37:56 -0400

    Update Board.java (#9)

--- Codes/Board.java
+++ Codes/Board.java
@@ -1,6 +1,27 @@
 
 public class Board {
   
-  //Here goes all the codes for the game to run.
-  //New edit
+static String[] board;
+
+    static void printBoard() {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                + board[1] + " | " + board[2]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                + board[4] + " | " + board[5]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                + board[7] + " | " + board[8]
+                + " |");
+        System.out.println("|---|---|---|");
+    }
+
+    static void initializeBoard() {
+        for (int a = 0; a < 9; a++) {
+            board[a] = String.valueOf(a + 1);
+        }
+    }
 }
