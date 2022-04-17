33a8fde9330accee3976ee796f7f14b281c68b13
chaudhea <98905501+chaudhea@users.noreply.github.com>
2022-04-17 16:29:35 -0400

    Deleted the comments

--- Codes/Game.java
+++ Codes/Game.java
@@ -2,8 +2,8 @@ public class Game
 {
     public static void main(String[] args) throws Exception 
     {
-       //Here goes all the codes to start the game.
-        // this is java code........
+      
+      
     }
 }
 
