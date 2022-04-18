eb1812f119d96a5b674349dc0bdb33c045976197
siwakota <98551780+siwakota@users.noreply.github.com>
2022-04-17 20:53:24 -0400

    Complete code for main class

--- Codes/Game.java
+++ Codes/Game.java
@@ -1,9 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Game 
 {
     public static void main(String[] args) throws Exception 
     {
-//added something
-
+        Board b1 = new Board();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to 3x3 TIC TAC TOE");
+        Board.board = new String[9];
+        Board.initializeBoard();
+        Board.printBoard();
+        while (true) {
+            System.out.print("Please enter 1 to enter the game or 0 to exit the game:  ");
+            try {
+                int val = sc.nextInt();
+                if (val == 0) {
+                    System.out.println("You have exited from the game. See you again.");
+                    break;
+                }
+                else if (val == 1) 
+                {
+                   b1.startGame();
+                }
+                else
+                {
+                    continue;
+                }
+            } 
+            catch (InputMismatchException e) 
+            {
+                sc.nextLine(); // Clears Buffer
+                continue;
+            }
+            sc.close();
+        }
     }
 }
 
