8acd51d25aa546888aa09f36345cbbc5252e5e1e
siwakota <98551780+siwakota@users.noreply.github.com>
2022-04-17 20:57:56 -0400

    Final codes added.

--- Codes/Game.java
+++ Codes/Game.java
@@ -1,16 +1,18 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Game 
-{
-    public static void main(String[] args) throws Exception 
-    {
-        Board b1 = new Board();
-        Scanner sc = new Scanner(System.in);
+public class Game {
+    public static void main(String[] args) throws Exception {
+        Board b1 = new Board(); // An object of Board class is created.
+
+        Scanner sc = new Scanner(System.in); // Scanner object to get user inputs.
+
         System.out.println("Welcome to 3x3 TIC TAC TOE");
-        Board.board = new String[9];
-        Board.initializeBoard();
-        Board.printBoard();
+        Board.board = new String[9]; // Size given to Static string variable
+        Board.initializeBoard(); // Loads the numbers into the board as position.
+
+        Board.printBoard(); // Prints the intial board with the positional numbers.
+
         while (true) {
             System.out.print("Please enter 1 to enter the game or 0 to exit the game:  ");
             try {
@@ -18,18 +20,12 @@ public class Game
                 if (val == 0) {
                     System.out.println("You have exited from the game. See you again.");
                     break;
-                }
-                else if (val == 1) 
-                {
-                   b1.startGame();
-                }
-                else
-                {
+                } else if (val == 1) {
+                    b1.startGame(); // Invoked startGame method of Board class.
+                } else {
                     continue;
                 }
-            } 
-            catch (InputMismatchException e) 
-            {
+            } catch (InputMismatchException e) {
                 sc.nextLine(); // Clears Buffer
                 continue;
             }
@@ -37,4 +33,3 @@ public class Game
         }
     }
 }
-
